d9b8d4beb9f12f8ecf3a8d26bcca602436f0cf58
xutingl <xutingl@seas.upenn.edu>
2025-08-07 03:43:36 +0000

    [Bug] >= --> > in checking if we want to do deep buffer with standard iter

--- sarathi-lean/sarathi/model_executor/models/llama.py
+++ sarathi-lean/sarathi/model_executor/models/llama.py
@@ -941,7 +941,7 @@ class LlamaModel(nn.Module):
                     self.exited_rates[1] += len(seq_ids_in_batch)
 
                     # If there are enough requests in the `deep_buffer`, we will add them to the current deep iteration i.e. concate them to current hidden_states.
-                    if len(self.deep_buffer) >= self.get_adaptive_rebatching_threshold(hidden_states.size(0), rebatching_ee_factor):
+                    if len(self.deep_buffer) > self.get_adaptive_rebatching_threshold(hidden_states.size(0), rebatching_ee_factor):
                         # Take hidden states from `deep_buffer`
                         deep_buffer_hidden_states, deep_buffer_seq_ids, deep_buffer_positions = self.deep_buffer.take_hidden_states()
 
